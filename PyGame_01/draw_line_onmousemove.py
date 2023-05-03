eb9f9dfa4c086f869d9cab03159d7876208da7a1
MASHIRO\Mashiro <youho98zzz@gmail.com>
2023-05-03 23:58:04 +0900

    新しい勉強した内容をアップする

---
+++ PyGame_01/draw_line_onmousemove.py
@@ -0,0 +1,37 @@
+# draw_line_onmousemove.py
+import sys 
+import pygame 
+from pygame.locals import QUIT, \
+    MOUSEBUTTONDOWN, MOUSEMOTION, MOUSEBUTTONUP
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+def main():
+    mousepos = []
+    mousedown = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                mousedown = True
+            elif event.type == MOUSEMOTION:
+                if mousedown:
+                    mousepos.append(event.pos)
+            elif event.type == MOUSEBUTTONUP:
+                mousedown = False
+                mousepos.clear()
+            
+        screen.fill((255, 255, 255))
+        if len(mousepos) > 1:
+            pygame.draw.lines(screen, (255, 0, 0), False, mousepos)
+        
+        pygame.display.update()
+        FPSCLOCK.tick(10)
+
+if __name__ == '__main__':
+    main()
---
+++ PyGame_01/draw_lines1.py
@@ -0,0 +1,32 @@
+# draw_lines1.py
+import sys 
+from math import sin, cos, radians
+import pygame 
+from pygame.locals import QUIT 
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            
+        screen.fill((0, 0, 0))
+        pointlist0, pointlist1 = [], []
+        for theta in range(0, 720, 144):
+            rad = radians(theta)
+            pointlist0.append((cos(rad)*100 + 100, sin(rad)*100 + 150))
+            pointlist1.append((cos(rad)*100 + 300, sin(rad)*100 + 150))
+
+        pygame.draw.lines(screen, (255, 255, 255), True, pointlist0, 5)
+        pygame.draw.aalines(screen, (255, 255, 255), True, pointlist1)
+
+        pygame.display.update()
+        FPSCLOCK.tick(3)
+
+if __name__ == '__main__':
+    main()
